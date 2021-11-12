746084cfae0fe0c5e1542a419ad0fe9dc1dcad3e
DFkhin <fpogs30@gmail.com>
2021-11-12 23:17:38 +0800

    Initial commit

---
+++ resources/views/pages/landing.blade.php
@@ -0,0 +1,42 @@
+@extends('pages.base')
+
+
+@section('content')
+    
+
+<div class="jumbotron hero text-center">
+  <h2>Home <strong>Shoppe</strong></h2>
+</div>
+<div class="container">
+  <div class="card-deck">
+
+            <div class="card card1">
+              <div class="card-body">
+                    <h3>Product <span> reviews</span></h3>
+                </div>
+                <div class="col-mid-3 text-center">
+                    <div class="icon">
+                        <i class="fa fa-cogs"></i>  
+                    </div>
+                    <h3>Web<span> Development</span></h3>
+                </div>
+                <div class="col-mid-3 text-center">
+                    <div class="icon">
+                        <i class="fa fa-meetup"></i>  
+                    </div>
+                    <h3>Social<span> media</span></h3>
+                </div>
+                <div class="col-mid-3 text-center">
+                    <div class="icon">
+                        <i class="fa fa-video-camera"></i>  
+                    </div>
+                    <h3>Video <span> production</span></h3>
+                </div>
+                </div>
+              </div>
+
+  </div>
+</div>
+
+
+@endsection
